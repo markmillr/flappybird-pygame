f38f4f1ad73cd83ac5569a1f3aed814df6a91db8
markmillr <markmiller8@protonmail.ch>
2019-07-13 17:43:13 -0700

    single pipe appears

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pgzrun as pgz
 import pygame
+import random
 
 
 WIDTH = 1280
@@ -11,19 +12,26 @@ BACKGROUND_SCROLL_SPEED = 5
 GROUND_SCROLL_SPEED = 10
 BACKGROUND_LOOPING_POINT = WIDTH
 PSEUDOGRAVITY = 15
-
+PIPE_IMAGE='pipe.png'
+PIPE_SCROLL = -5
 
 class GameState():
     def __init__(self):
         self.groundscroll = 0
         self.backgroundscroll = 0
-        self.dt=1/30
 
 class Bird(Actor):
     def __init__(self, *args):
         Actor.__init__(self, *args)
         self.dy = 0
 
+class Pipe(Actor):
+    def __init__(self, *args):
+        Actor.__init__(self, *args)
+        self.x = WIDTH
+        self.y = random.randrange(3*HEIGHT/4, HEIGHT-10)
+
+pipe = Pipe('pipe.png')
 bird = Bird('bird.png')
 bird.x = WIDTH/2 - bird.width/2
 bird.y = HEIGHT/2 - bird.height/2
@@ -37,6 +45,8 @@ ground = pygame.transform.scale(ground, (WIDTH, 7*ground.get_height()))
 ground2 = ground
 
 
+
+
 bg = pygame.image.load('images/background.png')
 bg_height = bg.get_height()
 bg_width = bg.get_width()
@@ -46,7 +56,7 @@ bg = pygame.transform.scale(bg, (2*WIDTH, HEIGHT - ground.get_height()))
 bg2 = bg
 
 
-def move(dt):
+def bird_motion():
     
     if not keyboard.space:
         bird.y += PSEUDOGRAVITY
@@ -61,14 +71,17 @@ def update():
     gamestate.backgroundscroll = (gamestate.backgroundscroll + BACKGROUND_SCROLL_SPEED) % BACKGROUND_LOOPING_POINT
     print(gamestate.backgroundscroll)
     gamestate.groundscroll = (gamestate.groundscroll + GROUND_SCROLL_SPEED) % WIDTH
-    move(gamestate.dt)
+    pipe.x -= GROUND_SCROLL_SPEED
+    bird_motion()
 
 def draw():
     screen.clear()
     screen.blit(bg, (0 - gamestate.backgroundscroll,0))
     screen.blit(bg2, (WIDTH - gamestate.backgroundscroll, 0))
+    pipe.draw()
     screen.blit(ground, (0 - gamestate.groundscroll, HEIGHT-ground.get_height()))
     screen.blit(ground, (WIDTH - gamestate.groundscroll, HEIGHT-ground.get_height()))
     bird.draw()
+    
 
 pgz.go()
